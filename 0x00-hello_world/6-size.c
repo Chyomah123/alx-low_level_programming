52e075acb2238c6da38fbc8321682d4a2c1a3689
Favour Nwobodo-Charles <chiomah7890@gmail.com>
2022-06-30 18:26:41 -0700

    C Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,3 +19,4 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(e));
 	       printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(c));
 	       return (0);
+}
