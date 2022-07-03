cd05bd605d5aa55e0d571f27f00127612663e71b
Favour Nwobodo-Charles <chiomah7890@gmail.com>
2022-07-03 14:36:20 -0700

    inventions

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	n = '0';
+
+	while (n < '9')
+	{
+		for (m = '1'; m <= '9'; m++)
+		{
+			if (m != n)
+			{
+				putchar(n);
+				putchar(m);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
+
