c6d2353c80d2b14ccc8303a2632e68024f46f92a
Favour Nwobodo-Charles <chiomah7890@gmail.com>
2022-07-07 02:51:34 -0700

    checking for uppercase characters

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+int _isupper(int c);
+int _putchar(char c);
+int _isdigit(int);
+int mul(int, int);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int);
+void print_square(int);
+void fizz_buzz(void);
+void print_triangle(int);
