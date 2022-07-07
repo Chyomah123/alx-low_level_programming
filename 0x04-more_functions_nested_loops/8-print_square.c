5aa11ea73593bdf6a429b25c6feb8695aeb96022
Favour Nwobodo-Charles <chiomah7890@gmail.com>
2022-07-07 07:16:11 -0700

    printing a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - prints a square based on the number inputted
+ * @size: the size of the square in digits
+ *
+ */
+void print_square(int size)
+{
+	char c;
+	int i;
+	int j;
+
+	c = '#';
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar(c);
+		}
+	}
+}
