b7016e37319edf89e836a5805c4754d56bcb6d82
Favour Nwobodo-Charles <chiomah7890@gmail.com>
2022-07-12 15:14:38 -0700

    true lies

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character in a string
+ * str: the string inputted
+ */
+void puts2(char *str)
+{
+	int i;
+	char p;
+
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		if (i % 2 == 0)
+		{       p = *(str + i);
+			_putchar(p);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
