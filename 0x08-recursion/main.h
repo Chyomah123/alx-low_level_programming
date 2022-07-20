d93069370eb45bb9ea86bf5a7273812339958451
Favour Nwobodo-Charles <chiomah7890@gmail.com>
2022-07-20 09:26:31 -0700

    printing a string

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+void _puts_recursion(char *);
+int _putchar(char c);
+void _print_rev_recursion(char *);
+int _strlen_recursion(char *);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int sqrt_recursive(int, int, int);
+int _sqrt_recursion(int);
+int _prime_number(int, int);
+int is_prime_number(int);
+int is_palindrome_rec(char *, int, int);
+int is_palindrome(char *);
